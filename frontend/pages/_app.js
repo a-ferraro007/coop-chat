7f9fd22a75ddcfeeb4c7a5d3f8cf82584ab13f06
Anthony Ferraro <adferra24@gmail.com>
2021-06-22 23:36:26 -0400

    started basic login/create page

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,26 @@
 import 'tailwindcss/tailwind.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>
+        {`
+          body {
+            background: linear-gradient(
+                70.12deg,
+                rgba(0, 147, 233, 0.5) 0%,
+                rgba(128, 208, 199, 0.5) 100%
+              ),
+              #ffffff;
+          }
+          .box-shadow {
+            box-shadow: 0px 0px 5px 0.5px rgba(0, 0, 0, 0.16);
+          }
+        `}
+      </style>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,28 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    fontFamily: {
+      sans: 'Oswald, sans-serif'
+    },
+    extend: {
+      colors: {
+        'primary-dark': '#373F47',
+        'login-bg-blue': 'rgba(0, 147, 233, .5)',
+        'login-bg-green': 'rgba(128, 208, 99, .5)'
+      },
+      height: {
+        400: '400px'
+      },
+      minHeight: {
+        400: '400px'
+      },
+      width: {
+        665: '565px'
+      },
+      borderRadius: {
+        60: '60px'
+      }
+    }
   },
   variants: {
     extend: {}
